80fd4ace1c107f79a657d7678a1cb32bab61ca59
Simka-for <58855519+simka-for@users.noreply.github.com>
2020-02-20 17:04:46 +0300

    init metod

---
+++ src/main/java/DatabaseConfig.java
@@ -0,0 +1,23 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class DatabaseConfig {
+    public static void init(){
+
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().
+                configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+        Session session = sessionFactory.openSession();
+
+        Teacher teacher1 = session.get(Teacher.class, 1);
+        System.out.println("\n" +teacher1.toString() + "\n");
+
+        sessionFactory.close();
+    }
+}
